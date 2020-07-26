5111ebebf2cc2eeeb8bf72f9b766cbc740e96205
Alexander Reany <Alex.Reany@capstonelogistics.com>
2020-07-26 14:16:17 -0400

    Put and Patch Endpoints

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -4,24 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    [CourseTitleMustBeDifferentFromDescriptionAttribute(
-        ErrorMessage = "Title must be different from description.")]
-    public class CourseForCreationDto //: IValidatableObject
+    public class CourseForCreationDto : CourseForManipulationDto
     {
-        [Required(ErrorMessage = "You should fill out a title")]
-        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
-        public string Title { get; set; }
-        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
-        public string Description { get; set; }
-
-        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        //{
-        //    if (Title == Description)
-        //    {
-        //        yield return new ValidationResult(
-        //            "The provide description should be different from the title",
-        //            new[] { "CourseForCreationDTO" });
-        //    }
-        //}
     }
 }
