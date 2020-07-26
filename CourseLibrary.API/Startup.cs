7b811b6d8d684731dacae9afa7c8c3ad243301b5
Alexander Reany <Alex.Reany@capstonelogistics.com>
2020-07-26 10:25:02 -0400

    Validation, DTOs

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -1,12 +1,16 @@
+using AutoMapper;
 using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace CourseLibrary.API
 {
@@ -22,14 +26,64 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers();
-             
+           services.AddControllers(setupAction =>
+           {
+               setupAction.ReturnHttpNotAcceptable = true;
+           }).AddXmlDataContractSerializerFormatters()
+           .ConfigureApiBehaviorOptions(setupAction =>
+           {
+               setupAction.InvalidModelStateResponseFactory = context =>
+               {
+                   // create a problem details object
+                   var problemDetailsFactory = context.HttpContext.RequestServices
+                        .GetRequiredService<ProblemDetailsFactory>();
+                   var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                       context.HttpContext,
+                       context.ModelState);
+
+                   // add additional info not added by default
+                   problemDetails.Detail = "See the errors field for details.";
+                   problemDetails.Instance = context.HttpContext.Request.Path;
+
+                   // find out which status code to use
+                   var actionExecutingContext =
+                        context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+                   //if there are modelstate errors & all arguments were correcty
+                   // found/parsed we're dealing with validation errors
+                   if ((context.ModelState.ErrorCount > 0) &&
+                        (actionExecutingContext?.ActionArguments.Count ==
+                        context.ActionDescriptor.Parameters.Count))
+                   {
+                       problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
+                       problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                       problemDetails.Title = "One or more validation errors occured";
+
+                       return new UnprocessableEntityObjectResult(problemDetails)
+                       {
+                           ContentTypes = { "application/problem+json" }
+                       };
+                   };
+
+                   // if one of the arguments wasn't correctly found / couldn't be parsed
+                   // we're dealing with null/unparseable input
+                   problemDetails.Status = StatusCodes.Status400BadRequest;
+                   problemDetails.Title = "One or more validation errors occured";
+                   return new BadRequestObjectResult(problemDetails)
+                   {
+                       ContentTypes = { "application/problem+json" }
+                   };
+               };
+           });
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
             services.AddDbContext<CourseLibraryContext>(options =>
             {
                 options.UseSqlServer(
-                    @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
+                    @"Server=(localdb)\ProjectsV13;Database=CourseLibraryDB;Trusted_Connection=True;");
             }); 
         }
 
@@ -39,6 +93,17 @@ namespace CourseLibrary.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later");
+                    });
+                });
             }
 
             app.UseRouting();
